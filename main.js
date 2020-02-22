2c1a9e9aa326167b2d4765c32d49bcd6ef997cff
MalenaLucero <luceromalena@hotmail.com>
2020-02-22 16:06:06 -0300

    main functionality works

--- main.js
+++ main.js
@@ -3,69 +3,94 @@ let convertionInstance = {}
 let currentConvertions = []
 let convertionsHistory = []
 
+const initializeEventListener = () =>{
+    const input = document.getElementById('word')
+    input.addEventListener('keyup', event => {
+        if(event.keyCode === 13) convert()
+    })
+}
+
 const convert = () =>{
+    word = document.getElementById('word').value
+    if(word !== ''){
+        clearOldData() 
+        currentConvertions.push({"originalWord": word})
+        allConvertions()
+        printConvertions(word, currentConvertions, 'mainContainer')
+        convertionsHistory.push(currentConvertions)
+        printHistory()
+    } 
+}
+
+const clearOldData = () =>{
     innerHTMLCleaner('mainContainer')
     innerHTMLCleaner('historyContainer')
     currentConvertions = []
-    word = document.getElementById('word').value
-    currentConvertions.push({"word": word})
     inputCleaner()
-    toAllCaps()
-    toLowerCase()
-    toLowerCaseWithHyphens()
-    printConvertions(word, currentConvertions, 'mainContainer')
-    convertionsHistory.push(currentConvertions)
-    printHistory()
-    console.log(convertionsHistory)
 }
 
-const toAllCaps = () =>{
-    const convertedWord = word.toUpperCase()
-    convertionInstance = new convertion("ALL CAPS: ", convertedWord)
+//convertion managers
+const allConvertions = () =>{
+    newConvertion('ALL CAPS:', toAllCaps(word))
+    newConvertion('lower case:', toLowerCase(word))
+    newConvertion('lower case with hyphens:', toLowerCase(replaceSpacesWithHyphens(word)))
+}
+
+const newConvertion = (description, convertedWord) =>{
+    convertionInstance = new convertion(description, convertedWord)
     currentConvertions.push(convertionInstance)
 }
 
-const toLowerCase = () =>{
-    const convertedWord = word.toLowerCase()
-    convertionInstance = new convertion("to lower case: ", convertedWord)
-    currentConvertions.push(convertionInstance)
+//convertion constructor
+function convertion(type, convertedWord){
+    this.type = type
+    this.convertedWord = convertedWord
 }
 
-const toLowerCaseWithHyphens = () =>{
-    const convertedWord = word.toLowerCase().replace(/ /g, "-")
-    convertionInstance = new convertion("lower-case-hyphenated: ", convertedWord)
-    currentConvertions.push(convertionInstance)
-}
+//convertions
+const toAllCaps = wordToConvert => wordToConvert.toUpperCase()
 
+const toLowerCase = wordToConvert => wordToConvert.toLowerCase()
+
+const replaceSpacesWithHyphens = wordToConvert => wordToConvert.replace(/ /g, "-")
+
+// dynamic HTML generators
 const printConvertions = (word, convertions, containerId) =>{
-    const container = document.getElementById(containerId)
-    const title = document.createElement('h2')
+    const mainContainer = document.getElementById(containerId)
+    const innerContainer = document.createElement('article')
+    const title = document.createElement('h3')
     title.innerText = word
-    container.appendChild(title)
+    innerContainer.appendChild(title)
     convertions.forEach((convertion,index)=>{
         if(index !== 0){
             const convertionContainer = document.createElement('div')
             const type = document.createElement('p')
             type.innerText = convertion.type
-            const convertedWord = document.createElement('p')
+            const convertedWord = document.createElement('span')
             convertedWord.innerText = convertion.convertedWord
+            convertedWord
             convertionContainer.appendChild(type)
             convertionContainer.appendChild(convertedWord)
-            container.appendChild(convertionContainer)  
+            innerContainer.appendChild(convertionContainer)  
         }
     })
+    mainContainer.appendChild(innerContainer)
 }
 
 const printHistory = () =>{
     const historyContainer = document.getElementById('historyContainer')
-    const title = document.createElement('h3')
+    const title = document.createElement('h2')
     title.innerText = 'Convertion history'
     historyContainer.appendChild(title)
+    const contentContainer = document.createElement('div')
+    contentContainer.id = 'historyContentContainer'
+    historyContainer.appendChild(contentContainer)
     convertionsHistory.forEach(convertions=>{
-        printConvertions(convertions[0].word, convertions, 'historyContainer')
+        printConvertions(convertions[0].originalWord, convertions, 'historyContentContainer')
     })
 }
 
+//HTML cleaners
 const inputCleaner = () =>{
     const input = document.getElementById('word')
     input.value = ''
@@ -74,10 +99,5 @@ const inputCleaner = () =>{
 const innerHTMLCleaner = elementId =>{
     const element = document.getElementById(elementId)
     element.innerHTML = ''
-}
- 
-function convertion(type, convertedWord){
-    this.type = type
-    this.convertedWord = convertedWord
 }
 
